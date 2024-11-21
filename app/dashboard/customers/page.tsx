13c4568eb1b3f74881979e3925ebcbcebdd48191
BrantBart <brant.barton64@gmail.com>
2024-11-21 00:33:33 -0700

    16 push

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -2,7 +2,11 @@ import Search from '@/app/ui/search';
 import CustomersTable from '@/app/ui/customers/table'; // Import CustomersTable
 import { lusitana } from '@/app/ui/fonts';
 import { fetchFilteredCustomers } from '@/app/lib/data'; // Import fetchFilteredCustomers
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  title: 'Customers',
+};
 export default async function Page(props: { searchParams?: Promise<{ query?: string }> }) {
   const searchParams = await props.searchParams;
   const query = searchParams?.query || '';
@@ -12,12 +16,6 @@ export default async function Page(props: { searchParams?: Promise<{ query?: str
 
   return (
     <div className="w-full">
-      <div className="flex w-full items-center justify-between">
-        <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
-      </div>
-      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-        <Search placeholder="Search Customers..." />
-      </div>
       {/* Pass the fetched customers to CustomersTable */}
       <CustomersTable customers={customers} />
     </div>
